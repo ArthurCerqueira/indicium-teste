715f129bd1af4031e37db57233559423a920db22
Arthur Cerqueira <arthurcerqueira1206@gmail.com>
2022-05-16 23:35:19 -0300

    Segundo commit

--- src/components/Experiences/experiences.js
+++ src/components/Experiences/experiences.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styled from 'styled-components';
+import './experiences.css'
+
+const ListItem = styled.li`
+  text-align: center;
+  border-radius:5px;
+  padding: 15px;
+  border: 1px solid #ddd;
+  box-shadow: 0 0 6px 3px rgba(0,0,0,0.1);
+  margin-bottom: 15px;
+  transition: .3s;
+  font-weight: bold;
+  transition-delay: ${({delay}) => `${delay}ms`};
+  opacity: ${({isVisible}) => isVisible ? 1:0};
+  `;
+
+  const Main = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  `
+
+  const H1 = styled.h1 `
+    color: #7ac8ae;
+    font-size: 50px;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 800;
+  `
+
+  const Button = styled.button `
+    background: #7ac8ae;
+    border-radius: 20px;
+    padding: 10px;
+    border: 0px;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 500;
+    color:black;
+    &:hover {
+      color: white;
+    }
+  `
 
 const Experiences = () => {
+
+  const [listVisibility, setListVisibility] = useState(false);
+
+  function toggleListVisibility() {
+    setListVisibility(!listVisibility)
+  }
+
   return (
-    <div>E</div>
-  )
+    <Main>
+      <H1>Conheça minhas experiências!</H1>
+      <Button onClick={toggleListVisibility}>
+        Clique para revelar minha trajetória
+      </Button>
+      <ul className='listaexperiencias'>
+        {
+          [
+            'Auxiliar de cozinha - Camarão Express',
+            'Operador de telemarketing - Unike Corretora',
+            'Estágiario de TI - Pieta.tech',
+            'Futuro programador! - :)'
+          ].map((canal, indice) => (
+            <ListItem isVisible={listVisibility} delay={indice * 100}>
+              {canal}
+            </ListItem>
+          ))
+        }
+      </ul>
+    </Main>
+    );
 }
 
 export default Experiences
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import './home.css'
+import profile from './profile.png'
 
 const Home = () => {
   return (
-    <div>home</div>
+    <div className='home'>
+      <ul className='home1'>
+       <img src={profile} alt="Foto de perfil"/>
+       <ul className='lista'>
+         <li>Arthur Cerqueira Farias</li>
+         <li>18 anos</li>
+         <li>Estudante de Ciencias da Computação</li>
+         <li>Aspirante em desenvolvimento Front-end</li>
+         <li>Apaixonado por tecnologia</li>
+       </ul>
+       </ul>
+      <p className='quemsoueutitulo'>Um pouco sobre mim...</p>
+      <p className='quemsoueuparagrafo'>Me chamo <b>Arthur Cerqueira Farias</b> e sou natural de <b>São Paulo capital. </b>Em busca de uma vida melhor a seis anos atrás minha familia decidiu vir morar em <b>Florianópolis.</b>Mas bom acho que aqui não é o lugar ideal para falar da minha história. Mas seja bem vindo ao meu site!</p>
+    </div>
+    
+
   )
 }
 
